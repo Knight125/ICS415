9da557e5a08eb6f08b16adb57935829a4aec007e
Knight125 <knight2@hawaii.edu>
2015-09-27 20:56:51 -1000

    A08-2

--- A08/myScript.js
+++ A08/myScript.js
@@ -1,10 +1,29 @@
 /**
  * Created by Kara on 9/27/2015.
  */
+
+ occurrences = function(string, subString, allowOverlapping){
+
+  string+=""; subString+="";
+  if(subString.length<=0) return string.length+1;
+
+  var n=0, pos=0;
+  var step= allowOverlapping?1:subString.length;
+
+  while(true){
+    pos=string.indexOf(subString,pos);
+    if(pos>=0){ ++n; pos+=step; } else break;
+  }
+  return n;
+};
+//got from http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string
+
 $(document).ready(function(){
   $("#button").click(function(){
     $.get("FAQ.html", function(data, status){
-      alert("Data: " + data + "\nStatus: " + status);
+      var num = occurrences(data, "link");
+
+      $("#result").html(num);
     });
   });
   $("#hide").click(function(){
@@ -21,34 +40,49 @@ $(document).ready(function(){
 
   $("#hide2").click(function(){
     $("#two").hide();
+    $("#hide2").hide();
+    $("#show2").show();
   });
   $("#show2").click(function()
   {
     $("#two").show();
+    $("#show2").hide();
+    $("#hide2").show();
   });
 
   $("#hide3").click(function(){
     $("#three").hide();
+    $("#hide3").hide();
+    $("#show3").show();
   });
   $("#show3").click(function()
   {
     $("#three").show();
+    $("#show3").hide();
+    $("#hide3").show();
   });
 
   $("#hide4").click(function(){
     $("#four").hide();
     $("#hide4").hide();
+    $("#show4").show();
   });
   $("#show4").click(function()
   {
     $("#four").show();
+    $("#show4").hide();
+    $("#hide4").show();
   });
 
   $("#hide5").click(function(){
     $("#five").hide();
+    $("#hide5").hide();
+    $("#show5").show();
   });
   $("#show5").click(function()
   {
     $("#five").show();
+    $("#show5").hide();
+    $("#hide5").show();
   });
   });
